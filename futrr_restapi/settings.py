188d2178c7d0e9b645e3202e8372e2c2f742da2e
mowzlisre <mowzlisre@icloud.com>
2026-03-04 16:56:52 -0500

    PgSQL Integs

--- futrr_restapi/settings.py
+++ futrr_restapi/settings.py
@@ -108,11 +108,16 @@ WSGI_APPLICATION = 'futrr_restapi.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+import os
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
     }
 }
 
