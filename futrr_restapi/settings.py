1f85edd86863074ed7083e390cbe22830af0c4b7
mowzlisre <mowzlisre@icloud.com>
2026-03-04 17:04:40 -0500

    CSRF Correction

--- futrr_restapi/settings.py
+++ futrr_restapi/settings.py
@@ -27,7 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['api.futrr.app', 'localhost', '127.0.0.1']
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://api.futrr.app",
+    "https://api.futrr.app",
+    "http://localhost:3000",
+]
 # Application definition
 
 INSTALLED_APPS = [
