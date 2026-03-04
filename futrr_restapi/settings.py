f2efe9c150f673aa2b91d44c80f43a98ae352764
mowzlisre <mowzlisre@icloud.com>
2026-03-04 16:41:59 -0500

    Allowed HOSTS

--- futrr_restapi/settings.py
+++ futrr_restapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-__-ilb=(+ry33#r9#*!bbz*4$)no*)r03k(@7b_dmq%#u72ud$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['api.futrr.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
